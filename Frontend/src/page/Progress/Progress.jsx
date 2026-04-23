028820a6b34f32adc787beed6d8da12278098a39
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-04-23 12:57:45 +0700

    modified:   Frontend/src/page/Progress/Progress.jsx

--- Frontend/src/page/Progress/Progress.jsx
+++ Frontend/src/page/Progress/Progress.jsx
@@ -7,14 +7,22 @@ import { useState } from 'react'
 import TitleProgress from '../../components/Progress/TitleProgress';
 import ProgressSkill from '../../components/Progress/ProgressSkill';
 import Navbar from '../../components/Global/Navbar';
+import CareerOptions from '../../data/careerOptions.json'
 
 const Progress = () => {
   const { user } = useUser();
-  const {careerData} = useCareer();
+  const { careerData } = useCareer();
+
+  const getSkillbyCareerName = async () => {
+    const careerName = careerData.career_name
+
+    const skilll = CareerOptions.careers.find(career =>{career.name === careerName})
+  }
+
 
   return (
     <main className="ml-40 overflow-x-hidden pb-5">
-        <Navbar />
+      <Navbar />
       <div className="flex flex-col mt-8 ml-13">
         <TitleProgress />
       </div>
